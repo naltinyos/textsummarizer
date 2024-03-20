e3d6d0ca4575c8388d1fa237d30df41740bc47b1
unknown <venusquests@gmail.com>
2024-03-20 17:54:40 -0600

    update setup

--- setup.py
+++ setup.py
@@ -6,13 +6,23 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__="0.0.0"
 REPO_NAME="textsummarizer-project"
+AUTHOR_USER_NAME = "nyos"
 SRC_REPO="textsummarizer"
+AUTHOR_EMAIL = "nd"
 
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
+    author = AUTHOR_USER_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "nlp",
+    long_description=long_description,
+    long_description_content="text/markdown",
     url="https://github.com/naltinyos/textsummarizer",
+    project_urls={
+        "Bug tracker": "https://github.com/naltinyos/textsummarizer/issues",
+    },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
