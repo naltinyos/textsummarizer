fe7c586c0acd7ca1f81aae22f663493feacace7b
unknown <venusquests@gmail.com>
2024-03-20 12:44:26 -0600

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__="0.0.0"
+REPO_NAME="textsummarizer"
+SRC_REPO="textsummarizer"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    url="https://github.com/naltinyos/textsummarizer",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+
