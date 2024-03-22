4ed45f208da14334db70d213eb197d83efebd4d3
unknown <venusquests@gmail.com>
2024-03-22 16:31:59 -0600

    data update

--- src/textsummarizer/logging/__init__.py
+++ src/textsummarizer/logging/__init__.py
@@ -7,10 +7,12 @@ log_dir = "logs"
 log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
+
 logging.basicConfig(
-    Level = logging.INFO,
-    format = logging_str,
-    handlers = [
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
         logging.FileHandler(log_filepath),
         logging.StreamHandler(sys.stdout)
     ]
