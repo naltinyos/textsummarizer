edf042d94cfac1db491cef3d87056bc90d988edb
unknown <venusquests@gmail.com>
2024-03-25 23:29:36 -0600

    data validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from textsummarizer.logging import logger
-
+from textsummarizer.pipeline.S02_data_validation import DataValidationTrainingPipeline 
 from textsummarizer.pipeline.S01_data_ingestion import DataIngestionTrainingPipeline
 
 
@@ -14,4 +14,12 @@ except Exception as e:
     raise e
 
 
-
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
